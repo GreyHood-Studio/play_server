0a03140ad22db5b7353d22f40cd3c87061e15b0a
nowwoo91 <nowwoo91@gmail.com>
2018-05-14 23:00:26 +0900

    setup play server

--- setup.go
+++ setup.go
@@ -1,14 +1,1 @@
 package main
-
-import "github.com/spf13/viper"
-
-func readDefaultConfig() string {
-	viper.SetDefault("port", ":3000")
-
-	// config file name
-	viper.SetConfigName("config")
-
-	port := viper.Get("port").(string)
-
-	return port
-}
